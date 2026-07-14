2204a9eda93b6a1acb8e66f557bb85317b7a3522
Peter Tan Dat Le <petertl2@uci.edu>
2026-07-14 22:05:40 +0200

    ColabFold demo

---
+++ examples/demo_colabfold.py
@@ -0,0 +1,36 @@
+import pyrosetta
+import rosettalink
+from pyrosetta.rosetta.protocols.rosetta_scripts import XmlObjects
+
+#rosettalink.init('-fast_restyping -mute all')
+#rosettalink.init('')  
+rosettalink.init(set_logging_handler='interactive')
+
+
+pose = pyrosetta.pose_from_sequence("ACDEFGHIKLMNPQRSTVWY")
+for resnum in range(1, pose.total_residue() + 1):
+    pose.pdb_info().add_reslabel(resnum, "all")
+for resnum in range(1, 11):
+    pose.pdb_info().add_reslabel(resnum, "sculpted")
+
+xml_string = """
+<ROSETTASCRIPTS>
+
+    <MOVERS>
+        <ColabFold name="predict_structure" models="1,2,3,4,5" prefix_name="AF2_" replace_pose="1" msa_mode="single_sequence" cmd_header="singularity run --nv /home/folivieri/prosculpt/singularity_files/colabfold.sif colabfold_batch" work_dir="/home/d12-studenti/tandatp/test/" delete_dir="0">
+            <RMSD name="rmsd_all" reslabel="all"/>
+            <RMSD name="rmsd_sculpted" reslabel="sculpted"/>
+        </ColabFold>
+    </MOVERS>
+
+    <PROTOCOLS>
+        <Add mover="predict_structure"/>
+    </PROTOCOLS>
+
+</ROSETTASCRIPTS>
+""" 
+
+xml = XmlObjects.create_from_string(xml_string)
+protocol = xml.get_mover("ParsedProtocol") 
+protocol.apply(pose)
+pose.dump_pdb("/home/d12-studenti/tandatp/test/best_prediction.pdb")
